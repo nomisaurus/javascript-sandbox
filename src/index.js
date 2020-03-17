c5eb64d2a823b5324d63622550a6494049629de6
Noman ul Haq <mn.ulhaq@gmail.com>
2020-03-16 23:25:57 -0400

    variables and constants

--- src/index.js
+++ src/index.js
@@ -3,4 +3,29 @@
  * JavaScript Sandbox
  */
 
-console.log('Welcome to JavaScript Sandbox!');
+/** Comments */
+
+// single line comment
+
+/*
+    multiple line comment
+    add as many lines as you like
+*/
+
+/** Variables and Constants */
+
+// naming convention: name start with _ $ letter
+
+// declare a variable that cannot change with the 'const' keyword
+const message = 'Welcome to JavaScript Sandbox!';
+// message = 'Hello!'; // error!
+console.log(message);
+
+// declare a variable that can change with the 'let' keyword
+let total = 0;
+total = 10;
+console.log(`Your order total is $${total}.`);
+
+// avoid using the 'var' keyword to declare variables
+// console.log(oldMessage); // undefined, no error
+// var oldMessage = 'This is an old message!';
